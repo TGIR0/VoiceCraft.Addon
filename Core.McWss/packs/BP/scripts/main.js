e1f4eb5a86468c6b0ecb8b373024e06f32e330ec
SineVector241 <flyingbirdasuka1@gmail.com>
2025-07-22 12:38:47 +1000

    some tests.

--- Core.McWss/packs/BP/scripts/main.js
+++ Core.McWss/packs/BP/scripts/main.js
@@ -1,18 +1,10 @@
 import { NetDataWriter } from "./network/NetDataWriter";
 import { NetDataReader } from "./network/NetDataReader";
-import UTF8 from "./utf8";
 
-const str = "⌨⌨⌨aa⌨";
-console.warn(str);
-const bytes = UTF8.getBytes(str);
-const string = UTF8.getString(bytes, 0, bytes.byteLength);
-console.warn(string);
-
-/*
 const writer = new NetDataWriter();
 const value = 0.235896286;
 const value2 = 0.23246623896286;
-const value3 = "23saoignag";
+const value3 = "⌨⌨⌨aa⌨";
 console.warn(`Input: ${value}`);
 console.warn(`Input: ${value2}`);
 console.warn(`Input: ${value3}`);
@@ -21,4 +13,3 @@ writer.putDouble(value2);
 writer.putString(value3);
 const reader = new NetDataReader(writer);
 console.warn(`Output: ${reader.getDouble()}, Output 2: ${reader.getDouble()}, Output 3: ${reader.getString()}`);
-*/
